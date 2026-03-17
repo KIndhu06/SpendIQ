cc99f0620e89aef61f4ab4102be84a4649cd2060
Kondapaturu Indhu naga mani <kindhunagamani@gmail.com>
2026-03-17 10:43:21 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,12 +1,17 @@
 import pymysql
+import ssl
 
 def get_connection():
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    
     connection = pymysql.connect(
         host="mysql-48c3837-kindhunagamani-fce3.h.aivencloud.com",
         user="avnadmin",
-        password="YOUR_PASSWORD_HERE",
+        password="AVNS_hlTqrVhPxEdd4Xi4tiN",
         database="defaultdb",
         port=23940,
-        ssl={"ssl_mode": "REQUIRED"}
+        ssl=ssl_context
     )
     return connection
