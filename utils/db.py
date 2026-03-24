29861cae105907d67e2e463952e8192709dd6174
Kondapaturu Indhu naga mani <kindhunagamani@gmail.com>
2026-03-24 13:34:41 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,14 +1,19 @@
 import pymysql
 import streamlit as st
+import ssl
 
 def get_connection():
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    
     connection = pymysql.connect(
         host=st.secrets["mysql"]["host"],
         user=st.secrets["mysql"]["user"],
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"],
-        port=st.secrets["mysql"]["port"],
-        ssl_verify_cert=False,
-        ssl_verify_identity=False
+        port=int(st.secrets["mysql"]["port"]),
+        ssl=ctx,
+        connect_timeout=10
     )
     return connection
