4c854a6bbda7a995dc3e469b609ab5c975e8c2ef
Kondapaturu Indhu naga mani <kindhunagamani@gmail.com>
2026-03-24 11:07:11 +0530

    Update database connection to remove SSL context
    Removed SSL verification for database connection.

--- utils/db.py
+++ utils/db.py
@@ -1,17 +1,12 @@
 import pymysql
-import ssl
 
 def get_connection():
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-    
     connection = pymysql.connect(
         host="mysql-48c3837-kindhunagamani-fce3.h.aivencloud.com",
         user="avnadmin",
         password="AVNS_hlTqrVhPxEdd4Xi4tiN",
         database="defaultdb",
         port=23940,
-        ssl=ssl_context
+        ssl={"ca": ""}
     )
     return connection
