93829f79cba376033eed8edbe79860102a1f440b
Kondapaturu Indhu naga mani <kindhunagamani@gmail.com>
2026-03-24 13:38:50 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,19 +1,18 @@
 import pymysql
 import streamlit as st
-import ssl
 
 def get_connection():
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
-    
-    connection = pymysql.connect(
-        host=st.secrets["mysql"]["host"],
-        user=st.secrets["mysql"]["user"],
-        password=st.secrets["mysql"]["password"],
-        database=st.secrets["mysql"]["database"],
-        port=int(st.secrets["mysql"]["port"]),
-        ssl=ctx,
-        connect_timeout=10
-    )
-    return connection
+    try:
+        connection = pymysql.connect(
+            host=st.secrets["mysql"]["host"],
+            user=st.secrets["mysql"]["user"],
+            password=st.secrets["mysql"]["password"],
+            database=st.secrets["mysql"]["database"],
+            port=int(st.secrets["mysql"]["port"]),
+            ssl={"fake_flag_to_enable_ssl": True},
+            connect_timeout=10
+        )
+        return connection
+    except Exception as e:
+        st.error(f"DB Connection failed: {e}")  # shows real error on screen
+        raise
