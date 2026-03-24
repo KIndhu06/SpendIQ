f3f179c1050457dbdd2d248c899ca59b6f7f8f31
Kondapaturu Indhu naga mani <kindhunagamani@gmail.com>
2026-03-24 14:03:03 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,16 +1,16 @@
-import pymysql
+import mysql.connector
 import streamlit as st
 
 def get_connection():
     try:
-        connection = pymysql.connect(
+        connection = mysql.connector.connect(
             host=st.secrets["mysql"]["host"],
             user=st.secrets["mysql"]["user"],
             password=st.secrets["mysql"]["password"],
             database=st.secrets["mysql"]["database"],
             port=int(st.secrets["mysql"]["port"]),
-            ssl={"fake_flag_to_enable_ssl": True},
-            connect_timeout=10
+            ssl_disabled=False,
+            connection_timeout=10
         )
         return connection
     except Exception as e:
