2b880f43dc58a55553928d5da5214f54386d6d1a
Kondapaturu Indhu naga mani <kindhunagamani@gmail.com>
2026-03-24 13:54:10 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -9,10 +9,9 @@ def get_connection():
             password=st.secrets["mysql"]["password"],
             database=st.secrets["mysql"]["database"],
             port=int(st.secrets["mysql"]["port"]),
-            ssl={"fake_flag_to_enable_ssl": True},
             connect_timeout=10
         )
         return connection
     except Exception as e:
-        st.error(f"DB Connection failed: {e}")  # shows real error on screen
+        st.error(f"DB Connection failed: {e}")
         raise
